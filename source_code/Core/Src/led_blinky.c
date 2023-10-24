f34bf4b651ef3019e83fd9045b955272ebfa8a8e
ThienAn03 <hvt91vuhoangthienan@gmail.com>
2023-10-24 16:11:01 +0700

    Init for Ex1

--- source_code/Core/Src/led_blinky.c
+++ source_code/Core/Src/led_blinky.c
@@ -9,17 +9,17 @@
 
 
 
-void led_blinky(GPIO_TypeDef* port, uint16_t* pins, int time){
+void led_blinky(int time){
 	switch (status_led){
 	case INIT:
-		HAL_GPIO_WritePin(port, pins, RESET);
+		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
 		setTimer2(time);
 		status_led = Toggle;
 		break;
 	case Toggle:
 		if(timer2_flag == 1){
 			setTimer2(time);
-			HAL_GPIO_TogglePin(port, pins);
+			HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 		}
 		break;
 	default:
